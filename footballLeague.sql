f60947d4c3a838cc98c91c4e274bff0850bcf574
Kristijan Baro <baro.kristijan@gmail.com>
2018-12-03 15:26:18 +0100

    Add files via upload

--- footballLeague.sql
+++ footballLeague.sql
@@ -2,17 +2,11 @@ drop database if exists footballLeague;
 create database footballLeague;
 use footballLeague;
 
-create table league(
-  id int primary key auto_increment not null,
-  title varchar(50) not null,
-  since int not null,
-  start_ datetime not null,
-  end_ datetime not null
-);
 
 create table teams(
   id int primary key auto_increment not null,
-  league int not null,
+  grounds int not null,
+  games int not null,
   title varchar(50) not null,
   title2 varchar(50),
   country int not null,
@@ -21,10 +15,21 @@ create table teams(
   address varchar(50) not null
 );
 
+create table players(
+  id int primary key auto_increment not null,
+  teams int not null,
+  games int not null,
+  name varchar (50) not null,
+  surname varchar (50) not null,
+  position varchar (50) not null,
+  dateofBirth datetime
+);
+
 create table games(
   id int primary key auto_increment not null,
   round_ int not null,
-  ground int not null,
+  teams int not null,
+  grounds int not null,
   city int not null,
   start_ datetime not null,
   end_ datetime,
@@ -36,6 +41,8 @@ create table games(
 
 create table grounds(
   id int primary key auto_increment not null,
+  teams int not null,
+  games int not null,
   title varchar(50) not null,
   country int not null,
   city int not null,
@@ -44,22 +51,13 @@ create table grounds(
   address varchar(50) not null
 );
 
-create table games_teams(
-  id int primary key auto_increment not null,
-  games int not null,
-  teams int not null
-  );
 
-create table games_grounds(
-  id int primary key auto_increment not null,
-  games int not null,
-  grounds int not null
-  );
+alter table teams add foreign key (grounds) references grounds(id);
+alter table grounds add foreign key (teams) references teams(id);
+alter table players add foreign key (teams) references teams (id);
+alter table teams add foreign key (games) references games(id);
+alter table games add foreign key (teams) references teams(id);
+alter table games add foreign key (grounds) references grounds(id);
 
-alter table teams add foreign key (league) references league(id);
-alter table games_teams add foreign key (games) references games(id);
-alter table games_teams add foreign key (teams) references teams(id);
-alter table games_grounds add foreign key (games) references games(id);
-alter table games_grounds add foreign key (grounds) references grounds(id);
 
 
